6b9944e04edcd9004029d34a83f67f5dfeced8c6
Justin Nebo <nebojustin@gmail.com>
2018-10-08 12:29:23 +0100

    add async data calls and consume mock api

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -2,11 +2,8 @@ import * as actionTypes from '../actions/actionTypes';
 
 const courseReducer = (state = [], action) => {
   switch(action.type) {
-    case actionTypes.CREATE_COURSE:
-      return [
-        ...state,
-        Object.assign({}, action.course)
-      ];
+    case actionTypes.LOAD_COURSES_SUCCESS:
+      return action.courses;
 
     default:
       return state;
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+import thunk from 'redux-thunk';
 
 import rootReducers from '../reducers';
 
@@ -7,8 +8,8 @@ const configureStore = initialState => {
   return createStore(
     rootReducers,
     initialState,
-    applyMiddleware(reduxImmutableStateInvariant())
+    applyMiddleware(thunk, reduxImmutableStateInvariant())
   );
-};
+}; 
 
 export default configureStore;
