e8130fe1c9a2e11f4169e5eeee04e7810a330862
Justin Nebo <nebojustin@gmail.com>
2018-10-09 17:55:48 +0100

    setup production build process

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
-import configureStore from './store/configureStore';
+import configureStore from './store/configureStore'; // eslint-disable-line import/default
 
 import routes from './routes';
 import { loadCourses } from './actions/courseActions';
